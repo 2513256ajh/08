81ebf4785cc1b24d24f798d3ed3eaabd7c1e1596
2513256ajh <ajh13256@sookmyung.ac.kr>
2025-10-30 15:47:42 +0900

    02

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
 
 int main(int argc, char *argv[])
 {
-    int score[5];
-    int i;
+    int i, average;
+    int sum = 0;
+    int grade[SIZE];
     
-    score[0]=10;
-    score[1]=20;
-    score[2]=30;
-    score[3]=40;
-    score[4]=50;
+    printf("5명의 점수를 입력하세요.\n");
     
-    for(i=0;i<5;i++)
-    printf("score[%i]=%i\n",i, score[i]);
+    for(i=0;i<SIZE;i++)
+    {
+    scanf("%d", &grade[i]);
+    sum += grade[i];
+    }
+    #if 0
+    for(i=0;i<SIZE;i++)
+    printf("grade[%d]=%d\n", i, grade[i]);
+    #endif
+    
+    printf("성적 평균 : %i\n", sum/SIZE);
+    
     system("PAUSE");	
     return 0;
 }
